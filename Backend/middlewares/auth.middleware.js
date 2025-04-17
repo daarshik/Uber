03097c186bbca7fdbccba6f38f4b48ed950b1505
Daarshik-ccc <daarshik.d@ccc.training>
2025-04-17 23:21:59 +0530

    feat:User Authentication and Registration

--- Backend/middlewares/auth.middleware.js
+++ Backend/middlewares/auth.middleware.js
@@ -5,13 +5,13 @@ const jwt = require("jsonwebtoken");
 module.exports.authUser = async (req, res, next) => {
   const token = req.cookies.token || req.headers["authorization"].split(" ")[1];
   if (!token) {
-    return res.status(401).json({ message: "Unauthorized" });
+    return res.status(401).json({ message: "Unauthorized!" });
   }
 
   const isBlacklisted = await userModel.findOne({ token: token });
 
   if (isBlacklisted) {
-    return res.status(401).json({ message: "Unauthorized" });
+    return res.status(401).json({ message: "Unauthorized!" });
   }
 
   try {

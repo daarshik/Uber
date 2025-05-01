4a1dc093bf9d6f73ef38cb241a7a7fb8c2775938
Daarshik-ccc <daarshik.d@ccc.training>
2025-05-01 21:43:14 +0530

    feat: Created Login and Signup for user and captain

---
+++ Frontend/src/context/UserContext.jsx
@@ -0,0 +1,13 @@
+import React, { createContext } from "react";
+
+const UserDataContext = createContext();
+
+const UserContext = ({ children }) => {
+  return (
+    <div>
+      <UserDataContext.Provider>{children}</UserDataContext.Provider>
+    </div>
+  );
+};
+
+export default UserContext;
